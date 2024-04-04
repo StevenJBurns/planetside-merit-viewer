45572c860597396bbc87d58b3c83c3e1644ea875
SJB <stevenjburns@msn.com>
2024-04-04 14:46:36 -0500

    feat: re-implement original Silverlight files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MeritViewer
+{
+  public partial class MainPage : UserControl
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+    }
+  }
+}
